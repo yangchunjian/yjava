569473572ac66dbdecb1cc0700e220902db776c1
yangchunjian01 <yangchunjian01@58.com>
2022-01-23 23:07:19 +0800

    10进制转成2进制

--- yangchunjian/src/main/java/com/docbook/jian/test/Int2Bin.java
+++ yangchunjian/src/main/java/com/docbook/jian/test/Int2Bin.java
@@ -5,16 +5,8 @@ package com.docbook.jian.test;
  * @email 1091938307@qq.com
  * @created 2022-01-10
  * @timed 15:16
- */
-
-/**
  *
- int(123) -》  int 321
- 不能使用String Long
- +/-
- 无法反转 0
-
- result = result * 10 + temp;
+ * 10进制转成2进制
  */
 public class Int2Bin {
     public static void main(String[] args) {
