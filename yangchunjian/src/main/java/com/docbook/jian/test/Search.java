85a209ce5e3ef9e75cf5b285e1971c8b0618a209
yangchunjian01 <yangchunjian01@58.com>
2022-01-23 10:17:17 +0800

    面试

---
+++ yangchunjian/src/main/java/com/docbook/jian/test/Search.java
@@ -0,0 +1,27 @@
+package com.docbook.jian.test;
+
+/**
+ * @author Yang Chunjian
+ * @email 1091938307@qq.com
+ * @created 2022-01-10
+ * @timed 15:16
+ */
+
+/**
+ *
+ int(123) -》  int 321
+ 不能使用String Long
+ +/-
+ 无法反转 0
+
+ result = result * 10 + temp;
+ */
+public class Search {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
