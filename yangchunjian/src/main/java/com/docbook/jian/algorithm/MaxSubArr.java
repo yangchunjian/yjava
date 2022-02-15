335e6e19e4dedfb1dabe719f19a8f06b102ee6cd
yangchunjian01 <yangchunjian01@58.com>
2022-02-15 15:00:32 +0800

    盲点,面试题

--- yangchunjian/src/main/java/com/docbook/jian/algorithm/MaxSubArr.java
+++ yangchunjian/src/main/java/com/docbook/jian/algorithm/MaxSubArr.java
@@ -13,30 +13,30 @@ import java.util.List;
  * <p>
  * 最大子数组和
  * int[]数组中的最大子集，子集：数组中，数组下标连续的1个或者多个元素，形成的新数组；最大子集：和最大；例如：{1,2,3,4,5}其中{1}{1,2}{1,2,3}等是子集。要求写个方法，传入参数int[] i,返回参数：int类型最大子集的和
-
  */
 public class MaxSubArr {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-//        int[] arr = {1,-2,3,-4,5};
-//        int[] arr = {1,20,-1,20,5};
+//        int[] arr = {1,2,3,4,5};
+//        int[] arr = {1, -2, 3, -4, 5};
+        int[] arr = {1,20,-1,20,5};
         int ret = maxSubArr(arr);
-        System.out.println("arr="+ Arrays.toString(arr)+";ret="+ret);
+        System.out.println("arr=" + Arrays.toString(arr) + ";ret=" + ret);
 
     }
 
-    public static int maxSubArr(int[] arr){
-        int ret=0;
+    public static int maxSubArr(int[] arr) {
+        int ret = 0;
         List<Integer> temp = new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             int tempEle = arr[i];
-            for(int j=i+1;j<arr.length;j++){
-                tempEle = Math.max(tempEle,tempEle+arr[j]);
+            int tempJ = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                tempJ = tempJ+ arr[j];
+                tempEle = Math.max(tempEle, tempJ);
             }
-
-
             temp.add(tempEle);
         }
+        System.out.println("temp=" + Arrays.toString(temp.toArray()));
 
         ret = Collections.max(temp);
 
