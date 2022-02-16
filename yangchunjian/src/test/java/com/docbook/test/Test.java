a98c4e80c80586b613503fb1bad88afe1e808ae4
yangchunjian01 <yangchunjian01@58.com>
2022-02-16 18:18:44 +0800

    盲点,面试题

---
+++ yangchunjian/src/test/java/com/docbook/test/Test.java
@@ -0,0 +1,32 @@
+package com.docbook.test;
+
+import java.util.LinkedList;
+
+/**
+ * @author Yang Chunjian
+ * @email 1091938307@qq.com
+ * @created 2022-02-15
+ * @timed 23:18
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int star_idx = -1;
+        int i=0;
+
+        if(i==0){
+            star_idx = 10;
+        }else if(star_idx!=-1){
+            System.out.println("##");
+        }else {
+            System.out.println("##!!");
+        }
+//        StringBuffer sb = new StringBuffer();
+//        sb.
+        LinkedList sb = new LinkedList<>();
+        String ret = Integer.toBinaryString(12);
+        System.out.println("##ret="+ret);
+//        Long.parseUnsignedLong()
+//        ret.cha
+    }
+}
