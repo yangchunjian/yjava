c3014429f6172e58f88ac94b0fea8f2d03e936bf
yangchunjian01 <yangchunjian01@58.com>
2022-03-07 14:08:20 +0800

    测试java.lang.Integer

---
+++ yangchunjian/src/main/java/java/lang/Integer.java
@@ -0,0 +1,14 @@
+package java.lang;
+
+/**
+ * @author Yang Chunjian
+ * @email 1091938307@qq.com
+ * @created 2022-03-07
+ * @timed 12:28
+ */
+public class Integer {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
