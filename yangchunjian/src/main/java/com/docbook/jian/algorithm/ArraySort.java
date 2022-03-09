9794ce5a3fa3478d47f252d6cc10366296dc9137
yangchunjian01 <yangchunjian01@58.com>
2022-03-09 13:42:03 +0800

    搜索旋转排序数组

--- yangchunjian/src/main/java/com/docbook/jian/algorithm/ArraySort.java
+++ yangchunjian/src/main/java/com/docbook/jian/algorithm/ArraySort.java
@@ -9,7 +9,8 @@ package com.docbook.jian.algorithm;
 public class ArraySort {
 
 
-    //移位数组，快速查找到target角标
+    //移位数组，快速查找到target角标,
+    //思路：如果中间的数小于最右边的数，则右半段是有序的，若中间数大于最右边数，则左半段是有序的，我们只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了
     public static void main(String[] args) {
         int[] i = {8,9,10,11,1,2,3};
         int index = search(i,2);
