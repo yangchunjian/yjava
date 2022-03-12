7f4107aaeebb96903a0566d2a2e0caf6e60bc6c9
yangchunjian01 <yangchunjian01@58.com>
2022-03-12 08:48:25 +0800

    搜索旋转排序数组

---
+++ yangchunjian/src/test/java/com/docbook/test/Test6.java
@@ -0,0 +1,17 @@
+package com.docbook.test;
+
+/**
+ * @author Yang Chunjian
+ * @email 1091938307@qq.com
+ * @created 2022-03-11
+ * @timed 14:02
+ */
+public class Test6 {
+
+    public static void main(String[] args) {
+        int i = 7;
+
+        char j = (char)('1'+i);
+        System.out.println("#j="+j);
+    }
+}
