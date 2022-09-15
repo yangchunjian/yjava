68e4514cb90f2e48e779a7f26b7fe042f1b7f3dd
chunjian.yang <chunjian.yang@opay-inc.com>
2022-09-15 16:31:41 +0800

    思想

---
+++ yangchunjian/src/test/java/com/docbook/test/Test25.java
@@ -0,0 +1,22 @@
+package com.docbook.test;
+
+import java.util.Collections;
+
+/**
+ * @author Yang Chunjian
+ * @email 1091938307@qq.com
+ * @created 2022-02-15
+ * @timed 23:18
+ */
+public class Test25 {
+
+    public static void main(String[] args) {
+
+        Collections.reverse(null);
+
+    }
+
+
+
+
+}
