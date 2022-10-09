9ee07a8e1d8fc3a0a502a2e226e9a6a2404bc526
chunjian.yang <chunjian.yang@opay-inc.com>
2022-10-09 20:41:26 +0800

    思想

---
+++ yangchunjian/src/test/java/com/docbook/test/Test29.java
@@ -0,0 +1,49 @@
+package com.docbook.test;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * 栈转队列
+ * @author Yang Chunjian
+ * @email 1091938307@qq.com
+ * @created 2022-02-15
+ * @timed 23:18
+ */
+
+public class Test29 {
+
+
+    static Stack<Integer> s1 = new Stack<>();
+    static Stack<Integer> s2 = new Stack<>();
+
+
+    static LinkedList<Integer> l1;
+    public Test29(){
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static void add(Integer i){
+        s1.add(i);
+    };
+
+
+
+
+    public static void pop(){
+
+        s1.pop();
+
+    };
+
+    LinkedList<Integer> doLinked(){
+        return null;
+    }
+
+}
