ff237ffbc4a7a7966805fff9bbf91166afd97771
chunjian.yang <chunjian.yang@opay-inc.com>
2022-10-15 19:41:12 +0800

    思想

---
+++ yangchunjian/src/test/java/com/docbook/test/Test_Main.java
@@ -0,0 +1,14 @@
+package com.docbook.test;
+
+/**
+ * @author Yang Chunjian
+ * @email 1091938307@qq.com
+ * @created 2022-10-15
+ * @timed 19:40
+ */
+public class Test_Main {
+
+    public static void main(String[] args) {
+
+    }
+}
