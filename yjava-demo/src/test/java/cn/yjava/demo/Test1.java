53c88af6c7f5acc36f4743e9134c882d9dcfb632
yangchunjian <1091938307@qq.com>
2024-04-10 20:43:07 +0800

    A

--- yjava-demo/src/test/java/cn/yjava/demo/Test1.java
+++ yjava-demo/src/test/java/cn/yjava/demo/Test1.java
@@ -1,4 +1,39 @@
 package cn.yjava.demo;
 
 public class Test1 {
+
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 0; i < 300; i++) {
+            Thread a = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("ThreadA");
+                }
+            });
+            a.start();
+            a.join();
+            Thread b = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("ThreadB");
+                }
+            });
+
+            b.start();
+            b.join();
+            Thread c = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("ThreadC");
+                }
+            });
+
+            c.start();
+            c.join();
+        }
+
+
+    }
+
 }
